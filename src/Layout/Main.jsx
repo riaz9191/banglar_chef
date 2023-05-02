6326a77d30c1ee379e9ef6baf7e843b39ec4ab08
riaz9191 <riazahammed9191@gmail.com>
2023-05-02 20:32:08 +0600

    server side project setup and header footer added

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import Header from '../Pages/Shared/Header';
+import Footer from '../Pages/Shared/Footer';
+
+const Main = () => {
+    return (
+        <div>
+            <Header></Header>
+
+            
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Main;
